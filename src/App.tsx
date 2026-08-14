03a726041a6f68f017834985f00109a77a56b32a
t89774742040-source <t89774742040@gmail.com>
2026-08-14 12:12:09 +0300

    Исправить путь к значку в мобильной версии

--- src/App.tsx
+++ src/App.tsx
@@ -1103,7 +1103,7 @@ function App() {
     <div>
       <header>
         <button className="brand" onClick={() => go("home")}>
-          <img className="brand-icon" src="/icon-192.png" alt="" aria-hidden="true" /> НАШЕ НАСЛЕДИЕ
+          <img className="brand-icon" src={`${import.meta.env.BASE_URL}icon-192.png`} alt="" aria-hidden="true" /> НАШЕ НАСЛЕДИЕ
         </button>
         <button
           className="menu-toggle"
